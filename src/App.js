fa53541b9a6a665e4ae308f3a4a9daf580d19710
Tamadillo <tamadillo@users.noreply.github.com>
2019-08-07 21:25:01 +1000

    made some clouds
    used this generator for the background gradient
    
    https://www.cssmatic.com/gradient-generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [clouds, setClouds] = useState({ 1: false, 2: false, 3: false });
+
+  const cloudNext = () => {
+    setClouds({ 1: !clouds[1], 2: !clouds[2], 3: !clouds[3] });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="rail-1">
+        <img
+          src="/cloud_1.png"
+          className={`cloud ${clouds[1] && "App-cloud-1"}`}
+          alt="logo"
+        />
+      </div>
+      <div class="rail-2">
+        <img
+          src="/cloud_2.png"
+          className={`cloud ${clouds[2] && "App-cloud-2"}`}
+          alt="logo"
+        />
+      </div>
+      <div class="rail-3">
+        <img
+          src="/cloud_3.png"
+          className={`cloud ${clouds[3] && "App-cloud-3"}`}
+          alt="logo"
+        />
+      </div>
+      <button onClick={cloudNext}>do it</button>
     </div>
   );
 }
