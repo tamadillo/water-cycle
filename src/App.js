d3ab1342548a9ea780314a55563b2c2f17106ea3
Tamadillo <tamadillo@users.noreply.github.com>
2019-08-22 22:12:10 +1000

    added components for condensation, percipitation

--- src/App.js
+++ src/App.js
@@ -1,62 +1,131 @@
 import React, { useState } from "react";
 import "./App.css";
 
+const Evapo = () => (
+  <div style={{ position: "absolute", top: "63vh", left: "78vw" }}>
+    <img src="/evapo.png" style={{ width: "30px" }} />
+    <img src="/evapo.png" style={{ width: "30px" }} />
+    <img src="/evapo.png" style={{ width: "30px" }} />
+    <img src="/evapo.png" style={{ width: "30px" }} />
+    <img src="/evapo.png" style={{ width: "30px" }} />
+  </div>
+);
+
+const Clouds = () => (
+  <div className="sky">
+    <div className="rail-1">
+      <img src="/cloud_1.png" className="cloud App-cloud-1" alt="logo" />
+    </div>
+    <div className="rail-2">
+      <img src="/cloud_2.png" className="cloud App-cloud-2" alt="logo" />
+    </div>
+    <div className="rail-3">
+      <img src="/cloud_3.png" className="cloud App-cloud-3" alt="logo" />
+    </div>
+  </div>
+);
+
+const Percipitation = () => (
+  <div style={{ position: "absolute", top: "18vh", left: "22vw" }}>
+    <img
+      src="/cloud_1.png"
+      className="rain-cloud"
+      alt="logo"
+      style={{ left: "0px" }}
+    />
+    <img
+      src="/cloud_2.png"
+      className="rain-cloud"
+      alt="logo"
+      style={{ left: "200px" }}
+    />
+    <img
+      src="/cloud_3.png"
+      className="rain-cloud"
+      alt="logo"
+      style={{ left: "400px" }}
+    />
+    <img src="/rain.png" style={{ position: "absolute", top: "120px" }} />
+  </div>
+);
+const Runoff = () => <div>RUNOFF</div>;
+const Groundwater = () => <div>GROUNDWATER</div>;
+const Respiration = () => <div>RESPIRATION</div>;
+
 const data = [
   {},
   {
-    image: "show some image of evaporation"
+    show: Evapo
   },
   {
-    title: "evaporation",
+    title: "Evaporation",
     content:
-      "when the sun evaporates a liquid,it is the proccess of tearning a liquid into vapour.The vapuor stays in the atmosphear untill the vapuor cools down,and condenses into a cloud.",
-    top: "20vh",
-    left: "30vw"
+      "When the sun heats water, it provides the energy required to " +
+      "evaporate liquid water into a gas or vapor. " +
+      "The water vapor or gas stays in the atmosphear until the vapuor cools " +
+      "down, and condenses into a cloud.",
+    top: "6vh",
+    left: "58vw",
+    show: Evapo
   },
   {
-    image: "show some image of evaporation"
+    show: Clouds
   },
   {
-    title: "condensation",
+    title: "Condensation",
     content:
-      "when humid air comes in contact with a cold surfus,or cold air,it becomes little droplets of water.Sometimes the droplets of water condence into cloud, this is called condensation.Often when you use hot water the steam that comes off the water,can fog up a window or room ",
+      "When humid air comes in contact with a cold surface, or cold air, it " +
+      "becomes little droplets of water' this is called condensation. Sometimes " +
+      "the droplets of water can form a cloud. Often when you use hot water the " +
+      "steam that comes off the water, can fog up a window or room, this is also " +
+      "condensation.",
     top: "20vh",
-    left: "30vw"
+    left: "30vw",
+    show: Clouds
   },
   {
-    image: "show some image of evaporation"
-  },
-  {
-    title: "runoff",
-    content:
-      "A runoff is a small river or chanle of water that usualy appears when it rains alot.A runoff runs to the sea or alarge body of water such as a lake.Sometimes runoffs appear it is because the earth cant take enymore water.But sometimes runoffs are more perminant.",
-    top: "20vh",
-    left: "30vw"
-  },
-  {
-    image: "show some image of evaporation"
+    show: Percipitation
   },
   {
     title: "Precipitation",
     content:
-      "precipatation,is when the vapor in the sky condenses so much " +
-      "it becomes hevy enogh it fall. This often is in the form of rain, " +
+      "Precipatation, is when the vapor in the sky condenses so much " +
+      "it becomes hevy enogh it falls. This often is in the form of rain, " +
       "snow, hail or sleet.",
+    top: "30vh",
+    left: "54vw",
+    show: Percipitation
+  },
+  {
+    show: Runoff
+  },
+  {
+    title: "Runoff",
+    content:
+      "A runoff is a small river or chanle of water that usualy appears when it " +
+      "rains a lot. A runoff runs to the sea or alarge body of water such as a " +
+      "lake, Sometimes runoffs appear because the earth can't take anymore " +
+      "water. But sometimes runoffs are more perminant.",
     top: "20vh",
-    left: "30vw"
+    left: "30vw",
+    show: Runoff
+  },
+  {
+    show: Groundwater
   },
   {
     title: "Ground water",
     content:
-      "ounce it rains water soaks into the ground ,this is ground water." +
-      "Under the earth, there are lakes and rivers, that store water for meany years," +
-      "untill thay resurfus. Small rocks and pebbles help form the pools underground," +
-      "these pools are called aquifers.",
+      "Ounce it rains water soaks into the ground ,this is ground water." +
+      "Under the earth, there are lakes and rivers, that store water for " +
+      "meany years, untill thay resurfus. Small rocks and pebbles help form " +
+      "the pools underground, these pools are called aquifers.",
     top: "20vh",
-    left: "30vw"
+    left: "30vw",
+    show: Groundwater
   },
   {
-    image: "show some image of evaporation"
+    show: Respiration
   },
   {
     title: "Respiration",
@@ -64,15 +133,13 @@ const data = [
       "Respiration is when a livng being  such as a plant,an animal or a person," +
       "ctombind oxegen and glucose to make carbon dioxide, water and heatenergy.",
     top: "20vh",
-    left: "30vw"
-  },
-  {
-    image: ""
+    left: "30vw",
+    show: Respiration
   }
 ];
 
 const Card = ({ title, content, top, left }) => (
-  <div className="card" style={{ width: "18rem", top, left }}>
+  <div className="card" style={{ width: "42rem", top, left }}>
     {/* <img className="card-img-top" src="..." alt="Card image cap"> */}
     <div className="card-body">
       <h5 className="card-title">{title}</h5>
@@ -82,34 +149,7 @@ const Card = ({ title, content, top, left }) => (
   </div>
 );
 
-const Sky = ({ clouds }) => (
-  <div className="sky">
-    <div className="rail-1">
-      <img
-        src="/cloud_1.png"
-        className={`cloud ${clouds[1] && "App-cloud-1"}`}
-        alt="logo"
-      />
-    </div>
-    <div className="rail-2">
-      <img
-        src="/cloud_2.png"
-        className={`cloud ${clouds[2] && "App-cloud-2"}`}
-        alt="logo"
-      />
-    </div>
-    <div className="rail-3">
-      <img
-        src="/cloud_3.png"
-        className={`cloud ${clouds[3] && "App-cloud-3"}`}
-        alt="logo"
-      />
-    </div>
-  </div>
-);
-
 function App() {
-  const [clouds] = useState({ 1: true, 2: true, 3: true });
   const [index, setIndex] = useState(0);
 
   const handleKeyPress = event => {
@@ -138,9 +178,9 @@ function App() {
       autoFocus
       tabIndex="0"
     >
-      <Sky clouds={clouds} />
       <div>
         <div>{index}</div>
+        {data[index].show && data[index].show()}
         {data[index].title ? <Card {...data[index]} /> : <div />}
       </div>
     </div>
